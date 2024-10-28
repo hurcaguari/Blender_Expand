c1179b1ff288c548c10db8618c8af79be5e4b0f1
hurcaguari <hurcaguari@outlook.com>
2024-10-28 17:23:00 +0800

    原始库回档

--- docker_comm.bat
+++ docker_comm.bat
@@ -1,4 +1,7 @@
+docker login -u 
 pip freeze > requirements.txt
-docker rmi expand
-docker buildx build --platform linux/arm64 -t expand .
-docker save -o expand_arm64.tar expand 
+docker rmi expand:arm64
+docker buildx build --platform linux/arm64 -t expand:arm64 .
+docker save -o expand_arm64.tar expand:arm64 
+docker tag expand:arm64 docker.io/hurcaguari/expand:arm64
+docker push docker.io/hurcaguari/expand:arm64
